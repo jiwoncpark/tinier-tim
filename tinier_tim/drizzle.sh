2ca53874a4ad4c1d4a693422c5a12d46d2009b5a
jiwoncpark <jiwon.christine.park@gmail.com>
2020-06-18 11:06:00 -0700

    Use underscore for import

--- tinier_tim/drizzle.sh
+++ tinier_tim/drizzle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                                                                     
 #Run using iraf27 environment                
-cp ../tinier-tim/dri_psf.cl ./                                   
+cp ../tinier_tim/dri_psf.cl ./                                   
 echo "cl<dri_psf.cl; logout" | cl
 python ../tinier-tim/normalize_drizzled_psf.py
 rm dripsf.fits
